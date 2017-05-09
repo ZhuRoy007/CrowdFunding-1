b77b3ac554c98836e47f1d33297c81ce9b7c307d
Levin Yu <Levin2504@gmail.com>
2017-05-09 02:28:57 -0400

    个人资料查看页面

---
+++ profile.php
@@ -0,0 +1,9 @@
+<?php
+echo "个人资料";
+echo "
+            <a href=\"updateprofile.php\">
+                <button class=\"btn btn-primary\"
+                style=\"float: left;width: 30%;margin-left:20px;height: 30px;font-size: 15px;border-radius: 5px;color: whitesmoke;background-color: forestgreen\">Update Profile
+                </button>
+            </a>";
+//todo:
