be962cd0a3be48fa6d1a6f894c47f1c4b49972f1
ZhuRoy007 <rz1127@nyu.edu>
2017-05-11 07:25:21 -0400

    create user notify table and notification on donate

--- donateResult.php
+++ donateResult.php
@@ -31,6 +31,31 @@ project_id ='{$project_id}',
 fund_time = now(),  
 amount ='{$amount}',
 if_charged = 0;");
+
+/*Insert informatin to Table: Notification*/
+/*Define: type has categories: 1,user 2,project.   user has subtype:1,like 2,new 3.donate   project has 1.update 2.create 3.like  */
+$project_name = mysqli_query($con, "SELECT project_name FROM project WHERE project_id='{$project_id}';");
+
+$notify_message = $user_name. " has pledged a project: " . $project_name . " at " . date("Y-m-d H:i:s");
+$notify_message = (string)$notify_message;
+mysqli_query($con, "INSERT INTO notification (type, subtype, target_id, message, notify_time)
+    values('project', 'donate', {$row['user_id']},'$notify_message' , now());");
+
+$notify_id = mysqli_query($con, "SELECT notify_id FROM notification WHERE message = '{$notify_message}';");
+$notify_id = mysqli_fetch_array($notify_id);
+
+$who_followed = mysqli_query($con, "SELECT user_id from follow WHERE followed_id='{$user['user_id']}}';");
+
+if (mysqli_num_rows($who_followed) > 0) {
+    // output data of each row
+    while($tuple = mysqli_fetch_assoc($who_followed)) {
+        mysqli_query($con, "INSERT INTO user_notify SET user_id  ={$tuple['user_id']},notify_id ={$notify_id['notify_id']},if_read='0';");
+    }
+} else {
+    echo "No need to update user_notify table";
+}
+
+
 ?>
 
 <!DOCTYPE html>
